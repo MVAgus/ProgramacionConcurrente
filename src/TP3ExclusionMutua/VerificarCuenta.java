bd0fd70e1c1540e58fd49bd95d6fec7a4d73690e
mano_ <mano_@DESKTOP-JH32VBL>
2020-09-10 21:27:50 -0300

    Se analizo la ejecucion y me di cuenta de que hay que sincronizar los hilos.

---
+++ src/TP3ExclusionMutua/VerificarCuenta.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3ExclusionMutua;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mano_
+ */
+public class VerificarCuenta implements Runnable {
+    
+    private CuentaBanco cb = new CuentaBanco();
+    
+    private void HacerRetiro(int cantidad) throws InterruptedException{
+        
+        if (cb.getBalance() >= cantidad){
+            System.out.println(Thread.currentThread().getName() + " esta realizando un retiro de: "+cantidad+ ".");
+        Thread.sleep(1000);
+        cb.retiroBancario(cantidad);
+        
+        System.out.println(Thread.currentThread().getName()+" : Retiro realizado");
+        
+        System.out.println(Thread.currentThread().getName()+" Los fondos son de : "+cb.getBalance());
+    } else {
+            System.out.println("No hay suficiente dinero en la cuenta para relaizar el retiro del SR. "+Thread.currentThread().getName());
+            System.out.println("Su saldo actual es de "+cb.getBalance());
+            Thread.sleep(1000);
+        }
+    
+    }
+    
+    public void run(){
+        for (int i = 0; i <= 3; i++){
+            try {
+                
+                this.HacerRetiro(10);
+                if (cb.getBalance() < 0){
+                    System.out.println("La cuenta esta sobregirada");
+                }
+            } catch (InterruptedException ex){
+                Logger.getLogger(VerificarCuenta.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+    
+    
+}
